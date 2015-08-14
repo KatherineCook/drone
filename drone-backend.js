082df688b1a068bc5ac8ce95d409a768c1126368
V3505 (Conference ID) <v3505@cam.ac.uk>
2015-08-14 11:07:37 +0100

    drone navigation1

--- drone-backend.js
+++ drone-backend.js
@@ -34,13 +34,28 @@ function fly(robot) {
     });
     bot.drone.disableEmergency();
     bot.drone.ftrim();
+
     bot.drone.takeoff();
-    after(10*1000, function() {
+    after(5*1000, function (speed) {
+       bot.drone.forward(0.2);
+    });
+    after(7*1000, function(speed){
+        bot.drone.right(0.2);
+    });
+    after(9*1000, function(speed){
+        bot.drone.back(0.2);
+    });
+    after(11*1000, function(speed){
+        bot.drone.left(0.2);
+    });
+
+    after(13*1000, function() {
         bot.drone.land();
     });
-    after(15*1000, function() {
+    after(18*1000, function() {
         bot.drone.stop();
     });
+
 
 }
 
