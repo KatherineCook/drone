12a6074e37194f778a6ac4e74b35ed634c9b1ff1
V3505 (Conference ID) <v3505@cam.ac.uk>
2015-08-14 10:35:24 +0100

    Module 1

--- drone-backend.js
+++ drone-backend.js
@@ -11,10 +11,36 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav",{
+        driver: "ardrone",
+        connection: "ardrone"
+    })
     .on("ready", fly);
+
     
 // Fly the bot
+var bot;
 function fly(robot) {
+    bot=robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+    bot.nav.on("navdata", function(data) {
+        console.log("Altitude:", data);
+        if (altitude>1.5){
+            bot.drone.land();
+        }
+    });
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 
 }
 
